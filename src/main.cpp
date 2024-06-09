5fef598270a73068923a323d35994ba87d503d1b
shreyasnikte <shreyas.nikte@gmail.com>
2024-06-09 15:29:43 +0200

    init upload

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,15 @@ int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(2, OUTPUT);
+  Serial.begin(115200);
   int result = myFunction(2, 3);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  delay(1000);
+  Serial.println("Hello from the board");
 }
 
 // put function definitions here:
