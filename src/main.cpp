e9fabb53078e08e677bcb98f3bfeb1750035aaea
shreyasnikte <shreyas.nikte@gmail.com>
2024-06-09 16:44:34 +0200

    libraries added

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,22 @@
 #include <Arduino.h>
 
 // put function declarations here:
+int LED_BuiltIn = 2;
 int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(2, OUTPUT);
+  pinMode(LED_BuiltIn, OUTPUT);
   Serial.begin(115200);
   int result = myFunction(2, 3);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(LED_BuiltIn, HIGH);
   delay(1000);
   Serial.println("Hello from the board");
+  digitalWrite(LED_BuiltIn, 0x0);
+  delay(1000);
 }
 
 // put function definitions here:
