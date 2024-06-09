479423b923f6000b6e7c4836146bbf1f667c42c2
shreyasnikte <shreyas.nikte@gmail.com>
2024-06-09 16:50:24 +0200

    test

--- src/main.cpp
+++ src/main.cpp
@@ -17,10 +17,11 @@ void loop() {
   Serial.println("Hello from the board");
   digitalWrite(LED_BuiltIn, 0x0);
   delay(1000);
-  
+
 }
 
 // put function definitions here:
 int myFunction(int x, int y) {
   return x + y;
+  
 }
